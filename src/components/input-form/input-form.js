2de6d885c7c00565bd95f1972c0191a2c0d7aff6
LL Powe <powe2009@gmail.com>
2023-01-01 15:55:18 -0500

    scss for InputForm

--- src/components/input-form/input-form.js
+++ src/components/input-form/input-form.js
@@ -1,17 +1,23 @@
+import './input-form.styles.scss'
 
 const InputForm = ({ label, ...otherProps }) => {
-    const {id, type, name, value, onChange} = otherProps
+    // const {id, type, name, value, onChange} = otherProps
 
     return (
-        <div>
-            <label>{label}</label>
-            <input id={id} 
-                type={type}
-                name={name}
-                value={value}
-                onChange={onChange}
-                required
+        <div className='group'>
+            <input className='form-input'
+                {...otherProps}
             />
+            {/* apply shrink class on focus or input */}
+            {label && (
+                <label
+                    className={`${
+                        otherProps.value.length ? 'shrink' : ''
+                    } form-input-label`}
+                >
+                    {label}
+                </label>
+            )} 
         </div>
     )
 }
--- src/components/signup-form/signup-form.js
+++ src/components/signup-form/signup-form.js
@@ -32,7 +32,6 @@ const SignupForm = () => {
             // Athentication record is created in the Firebase
             const res = await createAuthUserWithEmailAndPassword(email, pass, confirmPass)
             handleReset()
-            // console.log('res', res)
         } catch (error) {
             alert(error.toString())
         }
@@ -48,43 +47,39 @@ const SignupForm = () => {
                 <h2>Sign Up</h2>
                 <form onSubmit={handleSubmit} onReset={handleReset}>
                     <InputForm id='displayName'
-                        label='Display Name: '
+                        label='Display Name'
                         type='text'
                         name='displayName'
                         value={displayName} 
                         onChange={handleChange}
                         minLength={2}
                     />
-                    <br />
 
                     <InputForm id='email'
-                        label='Email: '
+                        label='Email'
                         type='email'
                         name='email'
                         value={email}
                         onChange={handleChange}
                     />
-                    <br />
 
                     <InputForm id='pass'
-                        label='Password: '
+                        label='Password'
                         type='password'  
                         name='pass'
                         value={pass}
                         onChange={handleChange}
                         minLength={6}
                      />
-                    <br />
 
                     <InputForm id='confirmPass'
                         type='password'
-                        label='Confirm Password: '
+                        label='Confirm Password'
                         name='confirmPass'
                         value={confirmPass}
                         onChange={handleChange}
                         minLength={6}
                     />
-                    <br />
 
                     <div>
                         <button type='submit'>Submit</button>
